9b7a010696d32519046159ceb1d27f2ed7b37267
wanghai <944723226@qq.com>
2019-07-21 22:40:33 +0800

    创建游戏窗口

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -6,12 +6,15 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800,600))    #创建窗口
     pygame.display.set_caption("Alien Invasion")
-    #游戏主循环
+    bg_color = (230,230,230)
+    # 游戏主循环
     while True:
-        #监视键盘和鼠标事件
+        # 监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            #让最近绘制的屏幕可见
+            # 每次循环都重绘屏幕
+            screen.fill(bg_color)
+            # 让最近绘制的屏幕可见
             pygame.display.flip()
 run_game()
