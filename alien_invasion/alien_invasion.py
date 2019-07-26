eb76ffb4ddab304c10d85fb71fb65c9e84256a32
wanghai <944723226@qq.com>
2019-07-26 18:18:21 +0800

    创建模块，修改主函数

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf 
 
 def run_game():
     #初始化
@@ -19,6 +20,7 @@ def run_game():
     ship = Ship(screen)
     # 游戏主循环
     while True:
+        gf.check_events()
         # 监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
