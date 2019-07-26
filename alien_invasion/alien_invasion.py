b76814b02e94080c2749ce57ecefa28fb114d1c5
wanghai <944723226@qq.com>
2019-07-26 17:18:19 +0800

    添加设置的类，并改写主程序

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,12 +1,21 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     #初始化
     pygame.init()
+
+    """
     screen = pygame.display.set_mode((800,600))    #创建窗口
     pygame.display.set_caption("Alien Invasion")
     bg_color = (230,230,230)
+    """
+    #使用类进行初始化
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
     # 游戏主循环
     while True:
         # 监视键盘和鼠标事件
@@ -14,7 +23,8 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
             # 每次循环都重绘屏幕
-            screen.fill(bg_color)
+            #screen.fill(bg_color)
+            screen.fill(ai_settings.bg_color)
             # 让最近绘制的屏幕可见
             pygame.display.flip()
 run_game()
