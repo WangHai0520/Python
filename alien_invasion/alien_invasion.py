a0097bab90d34ed5bdb7e4d7a9d6bd84168cc358
wanghai <944723226@qq.com>
2019-07-26 18:08:32 +0800

    修复bug

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 def run_game():
     #初始化
     pygame.init()
@@ -14,7 +16,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    ship = Ship(screen)
     # 游戏主循环
     while True:
         # 监视键盘和鼠标事件
@@ -24,6 +26,7 @@ def run_game():
             # 每次循环都重绘屏幕
             #screen.fill(bg_color)
             screen.fill(ai_settings.bg_color)
+            ship.blitme()
             # 让最近绘制的屏幕可见
             pygame.display.flip()
 run_game()
